c3272356c5c31b09238d7eddf3f7900f6873f5d6
Rallge <geraldinelopz@gmail.com>
2021-07-07 02:01:20 +0200

    Hacer un insert

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 // Crear conexión pero nop hace nada
 
@@ -6,14 +6,14 @@ const sequelize = new Sequelize({
   host: "localhost",
   username: "geral",
   password: "geral",
-  database: "animales",
+  database: "pruebas",
   // colocas otra bd que no exista para probar los errores.
   dialect: "mysql",
 });
 
 // Probar conexión
 
-sequelize
+/* sequelize
   .authenticate()
   .then(() => {
     console.log("Conectado a la base de datos");
@@ -22,3 +22,48 @@ sequelize
     console.log("No me he podido conectar a la base de datos");
     console.log(err.message);
   });
+
+ */
+  // Schemas son las formas que tienen los objetos
+
+// SE ESTA PASANDO 3 COSAS
+
+  const Alumno = sequelize.define(
+    "Alumno",
+    {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+  nombre: {
+    type: DataTypes.STRING(32),
+    allowNull: false
+  },
+  apellidos:DataTypes.STRING(32),
+  dni: {
+    type: DataTypes.STRING(9),
+    unique: true
+  },
+  nota: {
+    type: DataTypes.DECIMAL(3,2),
+    // Colocar restriccion que no esta en la BD
+    validate: {
+      max: 10,
+      min: 0,
+    },
+  },
+},
+{
+  tableName: "alumnos",
+  timestamps: false,
+  });
+
+  // Metodo create
+
+  Alumno.create({
+    nombre: "Luis",
+    apellidos: "Martinez",
+    dni: "98989899T",
+    nota: 3,
+  }); 
